5f8655e1e5a1748e6663db0a5d3484929a88a12a
mfathurr <muhammad.fathur.rachman01@gmail.com>
2022-01-03 06:53:26 +0700

    create post controller, layouts/app and index file

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::latest()->when(request()->search, function($posts) {
+            $posts = $posts->where('title', 'like', '%'. request()->search . '%');
+        })->paginate(5);
+
+        return view('post.index', compact('posts'));
+    }
 }
